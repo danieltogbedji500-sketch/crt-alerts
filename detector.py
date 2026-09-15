a44999415aa8fd5bc550c1e2efbd830a0b7f4696
danieltogbedji500-sketch <danieltogbedji500@gmail.com>
2026-09-15 21:17:14 +0100

    Update detector.py

--- detector.py
+++ detector.py
@@ -139,7 +139,7 @@ def deduplicate_posts(posts):
 # Apify collector
 # ------------------------------------------------------------
 
-def collect_from_apify():
+def collect_from_apify(): from apify_collectors import collect_everything return collect_everything( SEARCH_QUERIES, PLATFORMS, )
     """
     Collection layer.
 
